aa1c5af52d05b36587346ab9f55bc732c0276e11
Shivam Shukla <shivamshukla@Shivams-MacBook-Air.local>
2024-01-13 00:58:03 +0530

    video player added

--- src/app/video/[videoID]/page.js
+++ src/app/video/[videoID]/page.js
@@ -1,14 +1,11 @@
-"use client"
+import VideoPlayer from "./videoPlayer";
 
-import { useParams } from 'next/navigation';
 
 export default function Video() {
-    const params      = useParams();
-    console.log(params);
-    const { videoID } = useParams();
+    
     return (
       <>
-        {videoID}
+        <VideoPlayer />
       </>
     )
 }
---
+++ src/app/video/[videoID]/videoPlayer.js
@@ -0,0 +1,42 @@
+"use client"
+import { useParams } from 'next/navigation';
+import YouTube from "react-youtube";
+
+export default function Content() {
+
+    const { videoID } = useParams();
+    
+
+    const checkElapsedTime = (e) => {
+      console.log(e.target.playerInfo.playerState);
+      const duration = e.target.getDuration();
+      const currentTime = e.target.getCurrentTime();
+      if (currentTime / duration > 0.95) {
+        setModalIsOpen(true);
+      }
+    };
+  
+    const opts = {
+      playerVars: {
+        autoplay: 1
+      }
+    };
+    
+  
+    return (
+      <div>
+        <div>
+          <div>
+            <YouTube
+              videoId={videoID}
+              className='youtube-player'
+              onStateChange={(e) => checkElapsedTime(e)}
+              opts={opts}
+            />
+          </div>
+        </div>
+      </div>
+    );
+}
+  
+  
