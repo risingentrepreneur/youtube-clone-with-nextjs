f0076c8f9be628bb5630b8f6baccebfe62574cc7
Shivam Shukla <shivamshukla@Shivams-MacBook-Air.local>
2024-01-14 00:26:31 +0530

    home link added on youtube logo

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faMagnifyingGlass, faMicrophone } from "@fortawesome/free-solid-svg-icons";
 import { faCircleUser, faCompass } from "@fortawesome/free-regular-svg-icons";
 import { useState, useEffect } from "react";
+import Link from 'next/link';
 
 
 export default function TopBar(props){
@@ -46,14 +47,16 @@ function ShowSearchBar({ setShowSearch }){
     return (
         <nav className = "top-bar">
             <div>
-                <Image
-                src = "/youtube-white-logo.png"
-                alt = "youtube-logo"
-                width = {0}
-                height = {0}
-                sizes = "100vh"
-                className = "logo"
-                />
+                <Link href="/">
+                    <Image
+                    src = "/youtube-white-logo.png"
+                    alt = "youtube-logo"
+                    width = {0}
+                    height = {0}
+                    sizes = "100vh"
+                    className = "logo"
+                    />
+                </Link>
             </div>
             <div className = "right-side">
                 <FontAwesomeIcon icon={faMagnifyingGlass} className='icon' onClick={ () => setShowSearch(true) } />
