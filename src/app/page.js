912e890525df112850ebaa126dccd53a3fade545
Shivam Shukla <shivamshukla@Shivams-MacBook-Air.local>
2024-01-14 20:11:56 +0530

    show hours in video duration if there is hours available

--- src/app/page.js
+++ src/app/page.js
@@ -121,7 +121,7 @@ function convertDurationStringToHHMMSS(durationString) {
   const formattedMinutes = minutes.toString().padStart(2, "0");
   const formattedSeconds = seconds.toString().padStart(2, "0");
 
-  return `${formattedMinutes}:${formattedSeconds}`;
+  return `${(formattedHours !== "00") ? formattedHours + ":" : ""}${formattedMinutes}:${formattedSeconds}`;
 }
 
 function condenseTimePassed(isoString) {
