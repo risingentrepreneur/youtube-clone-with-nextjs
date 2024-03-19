7dac044c91973088bb3b1cc8f30b3ec5a7a70ca4
Shivam Shukla <shivamshukla@Shivams-MacBook-Air.local>
2024-03-20 01:48:17 +0530

    added PWA functionalities

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
 import TopBar from '@/_components/header'
 import VideosList from '@/_components/videosList'
 import styles from '/public/assets/css/home.min.css'
+import RegisterServiceWorker from './registerServiceWorker.js'
+
 
 
 export default function Home() {
+  
+
   return (
     <main className={styles.main}>
       <TopBar showNavigationAndTopicMenu = { true } />
       <div className='list-on-home-page'> <VideosList /> </div>
+      <RegisterServiceWorker />
     </main>
   )
 }
---
+++ src/app/registerServiceWorker.js
@@ -0,0 +1,19 @@
+"use client"
+
+
+export default function RegisterServiceWorker() {
+    
+  //register service worker
+  if("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+     navigator.serviceWorker.register("sw.js").then(
+        function (registration) {
+          console.log("Service Worker registration successful with scope: ", registration.scope);
+        },
+        function (err) {
+          console.log("Service Worker registration failed: ", err);
+        }
+      );
+    });
+  }
+}
