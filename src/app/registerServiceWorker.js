443a28722a54813859b1945c1bf635330a6a503d
Shivam Shukla <shivamshukla@Shivams-MacBook-Air.local>
2024-03-20 02:03:35 +0530

    Navigator event added in client component with use effect

--- src/app/registerServiceWorker.js
+++ src/app/registerServiceWorker.js
@@ -1,19 +1,23 @@
 "use client"
 
+import { useEffect } from "react"
+
 
 export default function RegisterServiceWorker() {
     
-  //register service worker
-  if("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-     navigator.serviceWorker.register("sw.js").then(
-        function (registration) {
-          console.log("Service Worker registration successful with scope: ", registration.scope);
-        },
-        function (err) {
-          console.log("Service Worker registration failed: ", err);
-        }
-      );
-    });
-  }
+  useEffect(() => {
+    //register service worker
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+      navigator.serviceWorker.register("sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
 }
